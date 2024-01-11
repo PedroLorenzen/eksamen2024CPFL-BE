6f0d73c63f26fa517b0d8523df78dae12c506c2f
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2024-01-11 08:35:40 +0100

    create project

---
+++ src/main/java/com/example/eksamen2024cpflbe/Eksamen2024CpflBeApplication.java
@@ -0,0 +1,15 @@
+package com.example.eksamen2024cpflbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen2024CpflBeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Eksamen2024CpflBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamen2024cpflbe/Eksamen2024CpflBeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.eksamen2024cpflbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen2024CpflBeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
